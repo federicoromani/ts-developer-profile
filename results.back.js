fd4916156691565f133a8b673313e9cb0c6d9ce1
Federico <fromani@matchupglobal.com>
2022-03-30 12:03:02 -0300

    Test values

---
+++ results.back.js
@@ -0,0 +1,38 @@
+const BACK = [
+		{
+				"description": "Backend",
+				"techstack": [
+						"PHP Laravel",
+						"Node",
+						"MySQL"
+				],
+				"seniority": {
+						"experience": "1-2 años",
+						"category": "Junior"
+				}
+		},
+		{
+				"description": "Backend",
+				"techstack": [
+						"PHP Laravel",
+						"Node",
+						"MySQL"
+				],
+				"seniority": {
+						"experience": "2-6 años",
+						"category": "Semisenior"
+				}
+		},
+		{
+				"description": "Backend",
+				"techstack": [
+						"PHP Laravel",
+						"Node",
+						"MySQL"
+				],
+				"seniority": {
+						"experience": "+6 años",
+						"category": "Senior"
+				}
+		}
+	]
---
+++ results.front.js
@@ -0,0 +1,44 @@
+const FRONT = [
+	{
+			"description": "Frontend",
+			"techstack": [
+					"ES6",
+					"Typescript",
+					"VueJS",
+					"Pinia",
+					"UnoCSS"
+			],
+			"seniority": {
+					"experience": "1-2 años",
+					"category": "Junior"
+			}
+	},
+	{
+			"description": "Frontend",
+			"techstack": [
+					"ES6",
+					"Typescript",
+					"VueJS",
+					"Pinia",
+					"UnoCSS"
+			],
+			"seniority": {
+					"experience": "2-6 años",
+					"category": "Semisenior"
+			}
+	},
+	{
+			"description": "Frontend",
+			"techstack": [
+					"ES6",
+					"Typescript",
+					"VueJS",
+					"Pinia",
+					"UnoCSS"
+			],
+			"seniority": {
+					"experience": "+6 años",
+					"category": "Senior"
+			}
+	}
+]
